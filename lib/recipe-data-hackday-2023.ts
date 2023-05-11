1df51e4749ab51f757823bbad7bac20fc45d20d2
akash1810 <akash1810@users.noreply.github.com>
2023-05-11 20:50:09 +0100

    feat: Add DynamoDb tables

--- lib/recipe-data-hackday-2023.ts
+++ lib/recipe-data-hackday-2023.ts
@@ -1,11 +1,25 @@
-import { join } from "path";
-import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
-import { GuStack } from "@guardian/cdk/lib/constructs/core";
-import type { App } from "aws-cdk-lib";
-import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
+import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
+import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { Tags } from 'aws-cdk-lib';
+import type { App } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class RecipeDataHackday2023 extends GuStack {
-  constructor(scope: App, id: string, props: GuStackProps) {
-    super(scope, id, props);
-  }
+	constructor(scope: App, id: string, props: GuStackProps) {
+		super(scope, id, props);
+
+		new Table(this, 'RecipeTable', {
+			tableName: 'recipes',
+			partitionKey: { name: 'path', type: AttributeType.STRING },
+			sortKey: { name: 'recipeId', type: AttributeType.NUMBER },
+		});
+
+		new Table(this, 'RecipeEditsTable', {
+			tableName: 'recipes-edited',
+			partitionKey: { name: 'path', type: AttributeType.STRING },
+			sortKey: { name: 'recipeId', type: AttributeType.NUMBER },
+		});
+
+		Tags.of(this).add('App', 'recipe-data-hackday-2023');
+	}
 }
