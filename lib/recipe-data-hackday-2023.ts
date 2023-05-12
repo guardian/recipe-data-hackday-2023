bf5ac2717b395601a89a5fbd0c914d236fe4ab6f
Michel <64908337+michel-ds@users.noreply.github.com>
2023-05-12 09:35:16 +0100

    Add S3 bucket for storing recipes json dumps
    For people who prefer working with a flat file during the hack, this bucket can host those files.

--- lib/recipe-data-hackday-2023.ts
+++ lib/recipe-data-hackday-2023.ts
@@ -2,6 +2,7 @@ import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
 import { Tags } from 'aws-cdk-lib';
 import type { App } from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
 export class RecipeDataHackday2023 extends GuStack {
@@ -19,6 +20,8 @@ export class RecipeDataHackday2023 extends GuStack {
 			partitionKey: { name: 'path', type: AttributeType.STRING },
 			sortKey: { name: 'recipeId', type: AttributeType.NUMBER },
 		});
+
+		new s3.Bucket(this, 'StructuredRecipes'); 
 
 		Tags.of(this).add('App', 'recipe-data-hackday-2023');
 	}
